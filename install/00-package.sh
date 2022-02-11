dde200a062f1607f25e2f1956b0f92bd1436aff1
zeek0x <0h23k1.n40k1@gmail.com>
2022-02-12 00:14:40 +0900

    fix #3: Change to install FiraCode in package.

--- install/00-package.sh
+++ install/00-package.sh
@@ -6,13 +6,15 @@ function darwin () {
   # install brew
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew upgrade
-  brew install vim curl git zsh peco
+  brew install vim curl git zsh peco shellcheck
+  brew tap homebrew/cask-fonts
+  brew install --cask font-fira-code
 }
 
 function ubuntu () {
   apt-get update -y
   apt-get upgrade -y
-  apt-get install -y vim curl git zsh peco locales-all
+  apt-get install -y vim curl git zsh peco shellcheck fonts-firacode locales-all
 }
 
 case $(os) in
