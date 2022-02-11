3e622d22d490108db0405a28f780f9433f2e444e
zeek0x <0h23k1.n40k1@gmail.com>
2022-02-12 00:37:54 +0900

    fix #2: Change to install go by brew in Darwin.

--- install/10-go.sh
+++ install/10-go.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-curl -LO "https://get.golang.org/$(uname)/go_installer"
-chmod +x go_installer
-./go_installer
-rm go_installer
+function darwin () {
+  brew install go
+}
+
+function ubuntu () {
+  # The following code didn't work for Darwin.
+  # See: https://github.com/zeek0x/dotfiles/issues/2
+  curl -LO "https://get.golang.org/$(uname)/go_installer"
+  chmod +x go_installer
+  ./go_installer
+  rm go_installer
+}
+
+case $(os) in
+  darwin ) darwin ;;
+  ubuntu ) ubuntu ;;
+esac
