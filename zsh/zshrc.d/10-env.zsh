078ebf9a52220dbd08957d448fc3a39ecc08f350
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2024-10-13 22:02:38 +0900

    Improve 'less' envrioment

--- zsh/zshrc.d/10-env.zsh
+++ zsh/zshrc.d/10-env.zsh
@@ -15,7 +15,14 @@ export VISUAL='vi'
 export PAGER='less'
 
 # less
-export LESS='-i -M -R'
+export LESS='-g -i -M -R -S -W -z-4 -x4'
+export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
+export LESS_TERMCAP_md=$'\E[01;31m'      # Begins bold.
+export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
+export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
+export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
+export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
+export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
 
 # Go
 export GOPATH=$HOME/go
