7a0ec58a5d82f5bbaafedfb20a4d29afc7b89b61
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2025-07-09 14:24:10 +0900

    Update refzsh

--- zsh/zshrc.d/30-function.zsh
+++ zsh/zshrc.d/30-function.zsh
@@ -1,4 +1,9 @@
+function refzsh() {
+  rm -f '~/.zshrc.zwc'
+  . ~/.zshrc
+}
+
 function details() {
   [ -n "$1" ] && summary="<summary>$1</summary>\n\n" || summary=""
-  echo "<details>\n\n${summary}\`\`\`console\n$(cat -)\n\`\`\`\n\n</details>"
+  echo "<details>\n\n${summary}\`\`\`\n$(cat -)\n\`\`\`\n\n</details>"
 }
